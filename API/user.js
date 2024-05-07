a44b32d00ed4db0937d5d3fff30e6ba7bd937014
Samuel Finder <Samuel.Finder@bbd.co.za>
2024-05-07 10:47:34 +0200

    initial build of api

--- API/user.js
+++ API/user.js
@@ -1,16 +1,39 @@
 const express = require("express");
 const { Pool } = require("pg");
+const AWS = require("aws-sdk");
 
-const pool = new Pool({
-	user: "Spiderpedia_db",
-	password: "Spiderpedia1#",
-	host: "spiderpedia-postgres-db.c4n7thcq1lqm.eu-west-1.rds.amazonaws.com",
-	database: "SpiderpediaDB",
-	port: 5432,
-	ssl: {
-		rejectUnauthorized: false,
-	},
-});
+AWS.config.update({ region: "eu-west-1" });
+
+const secretsManager = new AWS.SecretsManager();
+let pool;
+
+async function initializePool() {
+	try {
+		const secretData = await secretsManager
+			.getSecretValue({
+				SecretId:
+					"arn:aws:secretsmanager:eu-west-1:179530787873:secret:spiderpedia_secrets-CiA3Se",
+			})
+			.promise();
+		const secret = JSON.parse(secretData.SecretString);
+
+		pool = new Pool({
+			user: secret.username,
+			password: secret.password,
+			host: "spiderpedia-postgres-db.c4n7thcq1lqm.eu-west-1.rds.amazonaws.com",
+			database: "SpiderpediaDB",
+			port: 5432,
+			ssl: {
+				rejectUnauthorized: false,
+			},
+		});
+	} catch (err) {
+		console.error("Error initializing database pool:", err);
+		process.exit(1);
+	}
+}
+
+initializePool();
 
 const router = express.Router();
 
