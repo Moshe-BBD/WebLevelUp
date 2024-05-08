39d8af5861419634014ad6d3dd30492ccd841a25
Samuel Finder <Samuel.Finder@bbd.co.za>
2024-05-08 16:06:46 +0200

    fixed migrations order

--- DB/migrations/sql/V010__insert_data_User.sql
+++ DB/migrations/sql/V010__insert_data_User.sql
@@ -1,7 +1,7 @@
 INSERT INTO "User" ("userId", "emailAddress", "username", "githubId")
 VALUES
   (1, 'john.doe@example.com', 'johndoe', 'johndoe_github_id'),
-  (2, 'jane.smith@example.com', 'janesmith', NULL),
+  (2, 'jane.smith@example.com', 'janesmith', 'jane_smith_github_id'),
   (3, 'alex.johnson@example.com', 'alexjohnson', 'alexjohnson_github_id'),
   (4, 'emma.wilson@example.com', 'emmawilson', 'emmawilson_github_id'),
   (5, 'michael.jones@example.com', 'michaeljones', NULL),
