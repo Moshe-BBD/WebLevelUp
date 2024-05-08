89d25175b6d285b98b25819a6307002d2a88a80d
Samuel Finder <Samuel.Finder@bbd.co.za>
2024-05-08 15:50:53 +0200

    added mock data to migrations

---
+++ DB/migrations/sql/V012_insert_data_SpiderFact.sql
@@ -0,0 +1,25 @@
+INSERT INTO "SpiderFact" ("spiderFactId", "spiderId", "factContent")
+VALUES
+  -- Facts for SpiderID = 1 (Phidippus audax)
+  (1, 1, 'Phidippus audax, also known as the daring jumping spider, is one of the most common and conspicuous jumping spiders in North America.'),
+  (2, 1, 'These spiders are known for their agile jumping ability and are often seen hunting for prey on walls, fences, and vegetation.'),
+  
+  -- Facts for SpiderID = 2 (Habronattus coecatus)
+  (3, 2, 'Habronattus coecatus is a species of jumping spider commonly found in North America.'),
+  (4, 2, 'These spiders have excellent vision and use their keen eyesight to locate and capture prey.'),
+  
+  -- Facts for SpiderID = 3 (Latrodectus mactans)
+  (5, 3, 'Latrodectus mactans, commonly known as the southern black widow, is a species of venomous spider found in the southeastern United States.'),
+  (6, 3, 'Female Latrodectus mactans spiders are known for their distinctive black coloration and red hourglass-shaped markings on the underside of their abdomen.'),
+  
+  -- Facts for SpiderID = 4 (Steatoda grossa)
+  (7, 4, 'Steatoda grossa, commonly known as the false black widow, is a species of spider in the family Theridiidae.'),
+  (8, 4, 'These spiders are often mistaken for the more dangerous black widow spider due to their similar appearance.'),
+  
+  -- Facts for SpiderID = 5 (Araneus diadematus)
+  (9, 5, 'Araneus diadematus, commonly known as the European garden spider, is a species of orb-weaving spider.'),
+  (10, 5, 'These spiders are known for their intricate orb webs, which they use to capture flying insects like flies and mosquitoes.'),
+  
+  -- Facts for SpiderID = 6 (Argiope aurantia)
+  (11, 6, 'Argiope aurantia, commonly known as the yellow garden spider, is a species of orb-weaving spider found throughout North America.'),
+  (12, 6, 'These spiders are often recognized by their bright yellow and black coloration and distinctive zigzag pattern, known as a stabilimentum, in the center of their webs.');
