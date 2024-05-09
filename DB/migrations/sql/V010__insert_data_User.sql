5a4416dbaf2549c98a373f6b504a45267cd4a6c1
Samuel Finder <Samuel.Finder@bbd.co.za>
2024-05-09 11:08:56 +0200

    change to migraton scripts

--- DB/migrations/sql/V010__insert_data_User.sql
+++ DB/migrations/sql/V010__insert_data_User.sql
@@ -1,8 +1,8 @@
-INSERT INTO "User" ("userId", "emailAddress", "username", "githubId")
+INSERT INTO "User" ("emailAddress", "username", "githubId")
 VALUES
-  (1, 'john.doe@example.com', 'johndoe', 'johndoe_github_id'),
-  (2, 'jane.smith@example.com', 'janesmith', 'jane_smith_github_id'),
-  (3, 'alex.johnson@example.com', 'alexjohnson', 'alexjohnson_github_id'),
-  (4, 'emma.wilson@example.com', 'emmawilson', 'emmawilson_github_id'),
-  (5, 'michael.jones@example.com', 'michaeljones', NULL),
-  (6, 'sophia.brown@example.com', 'sophiabrown', 'sophiabrown_github_id');
+  ('john.doe@example.com', 'johndoe', 'johndoe_github_id'),
+  ('jane.smith@example.com', 'janesmith', 'jane_smith_github_id'),
+  ('alex.johnson@example.com', 'alexjohnson', 'alexjohnson_github_id'),
+  ('emma.wilson@example.com', 'emmawilson', 'emmawilson_github_id'),
+  ('michael.jones@example.com', 'michaeljones', "michaeljones_github_id"),
+  ('sophia.brown@example.com', 'sophiabrown', 'sophiabrown_github_id');
--- DB/migrations/sql/V014__insert_data_FavouriteSpider.sql
+++ DB/migrations/sql/V014__insert_data_FavouriteSpider.sql
@@ -1,12 +1,12 @@
-INSERT INTO "FavouriteSpider" ("favouriteSpiderID", "userId", "spiderId", "like")
+INSERT INTO "FavouriteSpider" ("userId", "spiderId", "like")
 VALUES
-  (1, 1, 1, 1),   
-  (2, 1, 7, 1),   
-  (3, 2, 2, 1),   
-  (4, 2, 8, 1),   
-  (5, 3, 3, 1),  
-  (6, 3, 9, 1),   
-  (7, 4, 4, 1),   
-  (8, 4, 10, 1),  
-  (9, 5, 5, 1),   
-  (10, 6, 6, 1);  
+  (1, 1, 1),   
+  (1, 7, 1),   
+  (2, 2, 1),   
+  (2, 8, 1),   
+  (3, 3, 1),  
+  (3, 9, 1),   
+  (4, 4, 1),   
+  (4, 10, 1),  
+  (5, 5, 1),   
+  (6, 6, 1);  
