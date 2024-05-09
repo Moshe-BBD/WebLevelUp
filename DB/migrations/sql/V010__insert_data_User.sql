3921dc061bb576881e46d231efd825f621eb8a4e
Samuel Finder <Samuel.Finder@bbd.co.za>
2024-05-09 11:18:39 +0200

    fixed script to change "" to ''

--- DB/migrations/sql/V010__insert_data_User.sql
+++ DB/migrations/sql/V010__insert_data_User.sql
@@ -4,5 +4,5 @@ VALUES
   ('jane.smith@example.com', 'janesmith', 'jane_smith_github_id'),
   ('alex.johnson@example.com', 'alexjohnson', 'alexjohnson_github_id'),
   ('emma.wilson@example.com', 'emmawilson', 'emmawilson_github_id'),
-  ('michael.jones@example.com', 'michaeljones', "michaeljones_github_id"),
+  ('michael.jones@example.com', 'michaeljones', 'michaeljones_github_id'),
   ('sophia.brown@example.com', 'sophiabrown', 'sophiabrown_github_id');
