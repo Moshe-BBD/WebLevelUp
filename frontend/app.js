03b070352b41320b855357a17849e109e54e08f9
Samuel Finder <Samuel.Finder@bbd.co.za>
2024-05-10 17:29:13 +0200

    mock data

--- frontend/app.js
+++ frontend/app.js
@@ -1,6 +1,80 @@
 let currentPage = 0;
 let totalPages = 0;
 
+// Stub data for spiders information
+const stubSpiders = [
+	{
+		spiderId: 4,
+		spiderName: "Steatoda grossa",
+		facts:
+			"Steatoda grossa, commonly known as the false black widow, is a species of spider in the family Theridiidae. These spiders are often mistaken for the more dangerous black widow spider due to their similar appearance.",
+		spiderImage: "steatoda_grossa.jpg",
+	},
+	{
+		spiderId: 10,
+		spiderName: "Holocnemus pluchei",
+		facts:
+			"They are also called the marbled cellar spider and are known for their delicate appearance and elongated body. Holocnemus pluchei are often found in dark, damp environments such as caves, cellars, and basements, where they construct messy, irregular webs to catch their prey.",
+		spiderImage: "holocnemus_pluchei.jpg",
+	},
+	{
+		spiderId: 6,
+		spiderName: "Argiope aurantia",
+		facts:
+			"Argiope aurantia, commonly known as the yellow garden spider, is a species of orb-weaving spider found throughout North America. These spiders are often recognized by their bright yellow and black coloration and distinctive zigzag pattern, known as a stabilimentum, in the center of their webs.",
+		spiderImage: "argiope_aurantia.jpg",
+	},
+	{
+		spiderId: 2,
+		spiderName: "Habronattus coecatus",
+		facts:
+			"Habronattus coecatus is a species of jumping spider commonly found in North America. These spiders have excellent vision and use their keen eyesight to locate and capture prey.",
+		spiderImage: "habronattus_coecatus.jpg",
+	},
+	{
+		spiderId: 9,
+		spiderName: "Pholcus phalangioides",
+		facts:
+			"They are commonly known as cellar spiders or daddy long-legs spiders due to their long, thin legs. Pholcus phalangioides are known for their unique behavior of vibrating their webs vigorously when disturbed, possibly to confuse or deter predators.",
+		spiderImage: "pholcus_phalangioides.jpg",
+	},
+	{
+		spiderId: 7,
+		spiderName: "Lycosa tarantula",
+		facts:
+			"They are also known as wolf spiders and are known for their hunting prowess, often stalking their prey instead of building webs. Despite their name, Lycosa tarantulas venom is not particularly harmful to humans, causing only mild irritation or discomfort.",
+		spiderImage: "lycosa_tarantula.jpg",
+	},
+	{
+		spiderId: 3,
+		spiderName: "Latrodectus mactans",
+		facts:
+			"Latrodectus mactans, commonly known as the southern black widow, is a species of venomous spider found in the southeastern United States. Female Latrodectus mactans spiders are known for their distinctive black coloration and red hourglass-shaped markings on the underside of their abdomen.",
+		spiderImage: "latrodectus_mactans.jpg",
+	},
+	{
+		spiderId: 1,
+		spiderName: "Phidippus audax",
+		facts:
+			"Phidippus audax, also known as the daring jumping spider, is one of the most common and conspicuous jumping spiders in North America. These spiders are known for their agile jumping ability and are often seen hunting for prey on walls, fences, and vegetation.",
+		spiderImage: "phidippus_audax.jpg",
+	},
+	{
+		spiderId: 5,
+		spiderName: "Araneus diadematus",
+		facts:
+			"Araneus diadematus, commonly known as the European garden spider, is a species of orb-weaving spider. These spiders are known for their intricate orb webs, which they use to capture flying insects like flies and mosquitoes.",
+		spiderImage: "araneus_diadematus.jpg",
+	},
+	{
+		spiderId: 8,
+		spiderName: "Pardosa amentata",
+		facts:
+			"These spiders are commonly found in grassy habitats and are skilled hunters, preying on small insects. Pardosa amentata females carry their egg sacs attached to their spinnerets until the eggs hatch, protecting them from predators.",
+		spiderImage: "pardosa_amentata.jpg",
+	},
+];
+
 function searchSpider() {
 	const searchText = document
 		.getElementById("search-input")
@@ -36,49 +110,14 @@ function updateArrows() {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-	const loginBtn = document.getElementById("login-btn");
-	const logoutBtn = document.getElementById("logout-btn");
-	const navUsername = document.getElementById("nav-username");
-	const bookContent = document.getElementById("book-content");
 	const IMAGE_BASE_URL =
 		"https://spiderpedia-bucket.s3.eu-west-1.amazonaws.com/";
-
-	function checkLoginAndUpdateContent() {
-		fetch("http://ec2-3-250-137-103.eu-west-1.compute.amazonaws.com:5000/user")
-			.then((response) => response.json())
-			.then((data) => {
-				if (data !== "Not logged in") {
-					loginBtn.style.display = "none";
-					logoutBtn.style.display = "inline";
-					navUsername.textContent = data.username;
-					navUsername.style.display = "inline";
-					bookContent.style.display = "block";
-					fetchSpidersInfo().then(displaySpiders);
-				} else {
-					loginBtn.style.display = "inline";
-					logoutBtn.style.display = "none";
-					navUsername.style.display = "none";
-					bookContent.style.display = "none";
-				}
-			});
-	}
-
-	checkLoginAndUpdateContent();
+	displaySpiders(stubSpiders);
 
 	document.getElementById("sort").addEventListener("change", function () {
-		fetchSpidersInfo().then((data) => {
-			if (data) {
-				const sortedData = sortData(data, this.value);
-				displaySpiders(sortedData);
-			}
-		});
+		const sortedData = sortData(stubSpiders, this.value);
+		displaySpiders(sortedData);
 	});
-
-	function fetchSpidersInfo() {
-		const apiUrl =
-			"http://ec2-3-250-137-103.eu-west-1.compute.amazonaws.com:5000/api/spiders-info";
-		return fetch(apiUrl).then((response) => response.json());
-	}
 
 	function sortData(data, sortOrder) {
 		if (sortOrder === "asc") {
@@ -91,6 +130,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	function displaySpiders(data) {
 		const book = document.querySelector(".book");
+		const bookContent = document.getElementById("book-content");
 		const existingPages = document.querySelectorAll(".page");
 		existingPages.forEach((page) => page.remove());
 
@@ -98,23 +138,24 @@ document.addEventListener("DOMContentLoaded", function () {
 			const page = document.createElement("article");
 			page.classList.add("page");
 			page.innerHTML = `
-    <section class="spider-page">
-		<article class="arrow-container">
-                <span class="arrow left-arrow" onclick="prevPage()"></span>
-				<span class="arrow right-arrow" onclick="nextPage()"></span>
-			</article>
-        <h2>${spider.spiderName}</h2>
-        <img src="${IMAGE_BASE_URL}${spider.spiderImage}" alt="${
+                <section class="spider-page">
+                    <article class="arrow-container">
+                        <span class="arrow left-arrow" onclick="prevPage()"></span>
+                        <span class="arrow right-arrow" onclick="nextPage()"></span>
+                    </article>
+                    <h2>${spider.spiderName}</h2>
+                    <img src="${IMAGE_BASE_URL}${spider.spiderImage}" alt="${
 				spider.spiderName
 			}" class="spider-image">
-        <p>${spider.facts || "No fact available"}</p>
-    </section>
-`;
+                    <p>${spider.facts || "No fact available"}</p>
+                </section>
+            `;
 			book.insertBefore(page, document.querySelector(".back-cover"));
 		});
 
 		totalPages = data.length;
 		showPage(currentPage);
+		bookContent.style.display = "block";
 	}
 
 	document.addEventListener("click", function (event) {
