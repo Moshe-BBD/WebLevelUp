3228a94a10cdd4925c86712cd359100108024e9f
Moshe-Brill <mosheahron.brill@vcontractor.co.za>
2024-05-12 04:38:53 +0200

    removed studSpiders

--- frontend/app.js
+++ frontend/app.js
@@ -2,6 +2,7 @@ let currentPage = 0;
 let totalPages = 0;
 let ascendingOrder = true;
 let userLoggedIn = false;
+let spiders = [];
 
 document.addEventListener("DOMContentLoaded", () => {
 	const loginButton = document.getElementById("login-button");
@@ -21,45 +22,34 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 	function fetchSpidersInfo() {
-		const apiUrl = "http://ec2-3-250-137-103.eu-west-1.compute.amazonaws.com:5000/api/spiders-info";
-		return fetch(apiUrl)
-			.then((response) => {
-				if (!response.ok) {
-					throw new Error('Network response was not ok');
-				}
-				return response.json();
-			})
-			.catch((error) => {
-				console.error('Failed to fetch spider info:', error);
-				return [];
-			});
-	}
+        const apiUrl = "http://ec2-3-250-137-103.eu-west-1.compute.amazonaws.com:5000/api/spiders-info";
+		spiders = fetch(apiUrl).then((response) => response.json());
+        return spiders;
+    }
 
 	function checkLoginAndRenderCards() {
-		fetchSpidersInfo().then((spiders) => {
-			fetch("http://ec2-3-250-137-103.eu-west-1.compute.amazonaws.com:5000/user")
-				.then((response) => response.json())
-				.then((data) => {
-					if (data !== "Not logged in") {
-						userLoggedIn = true;
-						loginButton.style.display = "none";
-						logoutButton.style.display = "inline";
-						navUsername.textContent = data.username;
-						navUsername.style.display = "inline";
-						carouselContainer.classList.remove('blur-effect');
-						carousel.style.filter = "none";
-						loginMessage.style.display = "none";
-					} else {
-						loginButton.style.display = "inline";
-						logoutButton.style.display = "none";
-						navUsername.style.display = "none";
-						carousel.style.filter = "blur(5px)";
-						loginMessage.style.display = "block";
-					}
-					renderSpiderCards(stubSpiders);
-				})
-				.catch(error => console.error('Error checking login status:', error));
-		});
+		fetchSpidersInfo();
+		fetch("http://ec2-3-250-137-103.eu-west-1.compute.amazonaws.com:5000/user")
+			.then((response) => response.json())
+			.then(data => {
+				if (data !== "Not logged in") {
+					userLoggedIn = true;
+					loginButton.style.display = "none";
+					logoutButton.style.display = "inline";
+					navUsername.textContent = data.username;
+					navUsername.style.display = "inline";
+					carouselContainer.classList.remove('blur-effect');
+					carousel.style.filter = "none";
+					loginMessage.style.display = "none";
+				} else {
+					loginButton.style.display = "inline";
+					logoutButton.style.display = "none";
+					navUsername.style.display = "none";
+					carousel.style.filter = "blur(5px)";
+					loginMessage.style.display = "block";
+				}
+				renderSpiderCards(spiders);
+			});
 	}
 
 	function renderSpiderCards(spiderArray) {
@@ -135,12 +125,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	function sortSpidersByName() {
 		if (ascendingOrder) {
-			stubSpiders.sort((a, b) => a.spiderName.localeCompare(b.spiderName));
+			spiders.sort((a, b) => a.spiderName.localeCompare(b.spiderName));
 		} else {
-			stubSpiders.sort((a, b) => b.spiderName.localeCompare(a.spiderName));
+			spiders.sort((a, b) => b.spiderName.localeCompare(a.spiderName));
 		}
 		ascendingOrder = !ascendingOrder;
-		renderSpiderCards(stubSpiders);
+		renderSpiderCards(spiders);
 	}
 
 	const searchButton = document.querySelector(".search-btn");
