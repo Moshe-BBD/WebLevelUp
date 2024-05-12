afd1d463fb613ff40b960524e9b984ac039c0826
Moshe-Brill <mosheahron.brill@vcontractor.co.za>
2024-05-12 02:44:44 +0200

    oauth logic added

--- frontend/app.js
+++ frontend/app.js
@@ -1,87 +1,63 @@
 let currentPage = 0;
 let totalPages = 0;
 let ascendingOrder = true;
+let userLoggedIn = false;
+
 document.addEventListener("DOMContentLoaded", () => {
+	const loginButton = document.getElementById("login-button");
+	const logoutButton = document.getElementById("logout-button");
+	const navUsername = document.getElementById("nav-username");
 	const carousel = document.getElementById("carousel");
-	const stubSpiders = [
-		{
-			spiderId: 4,
-			spiderName: "Steatoda grossa",
-			facts:
-				"Steatoda grossa, commonly known as the false black widow, is a species of spider in the family Theridiidae. These spiders are often mistaken for the more dangerous black widow spider due to their similar appearance.",
-			spiderImage: "steatoda_grossa.jpg",
-		},
-		{
-			spiderId: 10,
-			spiderName: "Holocnemus pluchei",
-			facts:
-				"They are also called the marbled cellar spider and are known for their delicate appearance and elongated body. Holocnemus pluchei are often found in dark, damp environments such as caves, cellars, and basements, where they construct messy, irregular webs to catch their prey.",
-			spiderImage: "holocnemus_pluchei.jpg",
-		},
-		{
-			spiderId: 6,
-			spiderName: "Argiope aurantia",
-			facts:
-				"Argiope aurantia, commonly known as the yellow garden spider, is a species of orb-weaving spider found throughout North America. These spiders are often recognized by their bright yellow and black coloration and distinctive zigzag pattern, known as a stabilimentum, in the center of their webs.",
-			spiderImage: "argiope_aurantia.jpg",
-		},
-		{
-			spiderId: 2,
-			spiderName: "Habronattus coecatus",
-			facts:
-				"Habronattus coecatus is a species of jumping spider commonly found in North America. These spiders have excellent vision and use their keen eyesight to locate and capture prey.",
-			spiderImage: "habronattus_coecatus.jpg",
-		},
-		{
-			spiderId: 9,
-			spiderName: "Pholcus phalangioides",
-			facts:
-				"They are commonly known as cellar spiders or daddy long-legs spiders due to their long, thin legs. Pholcus phalangioides are known for their unique behavior of vibrating their webs vigorously when disturbed, possibly to confuse or deter predators.",
-			spiderImage: "pholcus_phalangioides.jpg",
-		},
-		{
-			spiderId: 7,
-			spiderName: "Lycosa tarantula",
-			facts:
-				"They are also known as wolf spiders and are known for their hunting prowess, often stalking their prey instead of building webs. Despite their name, Lycosa tarantulas venom is not particularly harmful to humans, causing only mild irritation or discomfort.",
-			spiderImage: "lycosa_tarantula.jpg",
-		},
-		{
-			spiderId: 3,
-			spiderName: "Latrodectus mactans",
-			facts:
-				"Latrodectus mactans, commonly known as the southern black widow, is a species of venomous spider found in the southeastern United States. Female Latrodectus mactans spiders are known for their distinctive black coloration and red hourglass-shaped markings on the underside of their abdomen.",
-			spiderImage: "latrodectus_mactans.jpg",
-		},
-		{
-			spiderId: 1,
-			spiderName: "Phidippus audax",
-			facts:
-				"Phidippus audax, also known as the daring jumping spider, is one of the most common and conspicuous jumping spiders in North America. These spiders are known for their agile jumping ability and are often seen hunting for prey on walls, fences, and vegetation.",
-			spiderImage: "phidippus_audax.jpg",
-		},
-		{
-			spiderId: 5,
-			spiderName: "Araneus diadematus",
-			facts:
-				"Araneus diadematus, commonly known as the European garden spider, is a species of orb-weaving spider. These spiders are known for their intricate orb webs, which they use to capture flying insects like flies and mosquitoes.",
-			spiderImage: "araneus_diadematus.jpg",
-		},
-		{
-			spiderId: 8,
-			spiderName: "Pardosa amentata",
-			facts:
-				"These spiders are commonly found in grassy habitats and are skilled hunters, preying on small insects. Pardosa amentata females carry their egg sacs attached to their spinnerets until the eggs hatch, protecting them from predators.",
-			spiderImage: "pardosa_amentata.jpg",
-		},
-	];
+	const carouselContainer = document.getElementById('carousel-container');
+	const loginMessage = document.getElementById('loginMessage');
+	const IMAGE_BASE_URL = "https://spiderpedia-bucket.s3.eu-west-1.amazonaws.com/";
+
+	function fetchSpidersInfo() {
+		const apiUrl = "http://ec2-3-250-137-103.eu-west-1.compute.amazonaws.com:5000/api/spiders-info";
+		return fetch(apiUrl).then((response) => response.json());
+	}
+
+	const stubSpiders = fetchSpidersInfo();
+
+	loginButton.addEventListener('click', function() {
+        window.location.href = '/login';
+    });
+
+	logoutButton.addEventListener('click', function() {
+        window.location.href = '/logout';
+    });
+
+	function checkLoginAndRenderCards() {
+		fetch("http://ec2-3-250-137-103.eu-west-1.compute.amazonaws.com:5000/user")
+			.then((response) => response.json())
+			.then((data) => {
+				if (data !== "Not logged in") {
+					userLoggedIn = true;
+					loginButton.style.display = "none";
+        			logoutButton.style.display = "inline";
+					navUsername.textContent = data.username;
+					navUsername.style.display = "inline";
+					carouselContainer.classList.remove('blur-effect');
+					carousel.style.filter = "none";
+					loginMessage.style.display = "none";
+				} else {
+					loginButton.style.display = "inline";
+					logoutButton.style.display = "none";
+					navUsername.style.display = "none";
+					carousel.style.filter = "blur(5px)";
+					loginMessage.style.display = "block";
+				}
+			})
+			.catch(error => console.error('Error checking login status:', error));
+		renderSpiderCards(stubSpiders);
+	}
+
+	checkLoginAndRenderCards();
 
 	function renderSpiderCards(spiderArray) {
 		carousel.innerHTML = "";
 
 		spiderArray.forEach((spider, index) => {
-			const IMAGE_BASE_URL =
-				"https://spiderpedia-bucket.s3.eu-west-1.amazonaws.com/";
 			const card = document.createElement("article");
 			const img = document.createElement("img");
 			const title = document.createElement("h2");
@@ -95,6 +71,12 @@ document.addEventListener("DOMContentLoaded", () => {
 			desc.textContent = spider.facts;
 			likeBtn.classList.add("heart-btn");
 			likeBtn.innerHTML = "❤";
+			likeBtn.disabled = !userLoggedIn;
+			if (!userLoggedIn) {
+				likeBtn.classList.add('disabled'); // Add 'disabled' class for styling
+			} else {
+				likeBtn.classList.remove('disabled'); // Remove 'disabled' class if user is logged in
+			}
 			pageNumber.textContent = `Page ${index + 1} of ${spiderArray.length}`;
 			pageNumber.classList.add("page-number");
 
@@ -126,8 +108,6 @@ document.addEventListener("DOMContentLoaded", () => {
 		centerCard(carousel.children[0]);
 	}
 
-	renderSpiderCards(stubSpiders);
-
 	const sortByNameLink = document.querySelector('a[href="#about"]');
 	sortByNameLink.addEventListener("click", sortSpidersByName);
 
@@ -141,8 +121,8 @@ document.addEventListener("DOMContentLoaded", () => {
 		renderSpiderCards(stubSpiders);
 	}
 
-	const searchForm = document.getElementById("searchForm");
-	searchForm.addEventListener("submit", function (event) {
+	const searchButton = document.querySelector(".search-btn");
+	searchButton.addEventListener("submit", function (event) {
 		event.preventDefault();
 		searchSpider();
 	});
