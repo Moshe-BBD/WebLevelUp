41b8a198ec8911d4dd6c3526e26843cbf2834811
Samuel Finder <Samuel.Finder@bbd.co.za>
2024-05-12 18:28:32 +0200

    changed port number

--- frontend/app.js
+++ frontend/app.js
@@ -239,7 +239,7 @@ async function toggleSpiderLike(spiderId, likeBtn) {
 }
 function fetchUserId(username) {
 	fetch(
-		`http://ec2-3-250-137-103.eu-west-1.compute.amazonaws.com:5001/api/user-id/${username}`
+		`http://ec2-3-250-137-103.eu-west-1.compute.amazonaws.com:5002/api/user-id/${username}`
 	)
 		.then((response) => response.json())
 		.then((data) => {
